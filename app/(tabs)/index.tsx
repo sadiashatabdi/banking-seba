25e75e260b37aff5972849fb2a54955cf07984d4
Sarower Jahan <sarowerj@gmail.com>
2025-01-03 15:09:47 +0600

    Created card

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,12 +1,22 @@
-import React, {useEffect, useState} from 'react'
-import {View, Text, TouchableOpacity, Image} from "react-native"
-import {SafeAreaView} from "react-native-safe-area-context"
-import {router} from "expo-router"
+import React, { useEffect, useState } from "react";
+import { View, Text, TouchableOpacity, Image } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { router } from "expo-router";
+import Container from "@/components/Container";
+import CreditCard from "@/components/CreditCard";
 const DashboardScreen = () => {
- return (
-    <Text>Dashboard Screen</Text>
-        )
-    }
+  return (
+    <Container>
+      <CreditCard
+        cardNumber="1234567812345678"
+        cardHolder="SADIA SHATABDI"
+        expiryDate="12/30"
+      ></CreditCard>
+      <View>
+        <Text>Hello</Text>
+      </View>
+    </Container>
+  );
+};
 
-
-export default DashboardScreen
+export default DashboardScreen;
---
+++ components/CreditCard.tsx
@@ -0,0 +1,119 @@
+import React, { useState } from "react";
+import {
+  View,
+  Text,
+  StyleSheet,
+  ImageBackground,
+  Pressable,
+} from "react-native";
+
+import creditCard from "../assets/images/CreditCardBg.png";
+
+interface CreditCardProps {
+  cardNumber: string;
+  cardHolder: string;
+  expiryDate: string;
+}
+
+const CreditCard: React.FC<CreditCardProps> = ({
+  cardNumber,
+  cardHolder,
+  expiryDate,
+}) => {
+  const formatCardNumber = (number: string) =>
+    number.replace(/(\d{4})/g, "$1 ").trim();
+
+  const [balance, setBalance] = useState("Your Balance");
+
+  return (
+    <View style={styles.card}>
+      <ImageBackground
+        source={creditCard}
+        style={styles.background}
+        imageStyle={styles.imageStyle}
+      >
+        <View style={styles.topSection}>
+          <Pressable onPress={() => setBalance("2000.00 TK")}>
+            <Text style={styles.bankName}>{balance}</Text>
+          </Pressable>
+          <Text style={styles.bankName}>Banking Seba</Text>
+        </View>
+        <View style={styles.middleSection}>
+          <Text style={styles.cardNumber}>{formatCardNumber(cardNumber)}</Text>
+        </View>
+        <View style={styles.bottomSection}>
+          <View>
+            <Text style={styles.label}>Card Holder</Text>
+            <Text style={styles.cardHolder}>{cardHolder}</Text>
+          </View>
+          <View>
+            <Text style={styles.label}>Expires</Text>
+            <Text style={styles.expiryDate}>{expiryDate}</Text>
+          </View>
+        </View>
+      </ImageBackground>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  card: {
+    width: "100%",
+    aspectRatio: 1.6,
+    borderRadius: 12,
+    overflow: "hidden",
+    elevation: 5,
+    alignSelf: "center",
+  },
+  background: {
+    flex: 1,
+    justifyContent: "space-between",
+    padding: 16,
+  },
+  imageStyle: {
+    borderRadius: 12,
+  },
+  topSection: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+  },
+  bankName: {
+    fontSize: 18,
+    color: "#fff",
+    fontWeight: "bold",
+  },
+  middleSection: {
+    alignItems: "center",
+    marginTop: 70,
+  },
+  cardNumber: {
+    fontSize: 24,
+    color: "#fff",
+    letterSpacing: 2,
+  },
+  bottomSection: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "flex-end",
+  },
+  label: {
+    fontSize: 10,
+    color: "#FFFFFF",
+    textTransform: "uppercase",
+  },
+  cardHolder: {
+    fontSize: 16,
+    color: "#fff",
+    fontWeight: "bold",
+    marginTop: 4,
+  },
+  expiryDate: {
+    fontSize: 16,
+    color: "#fff",
+    fontWeight: "bold",
+    marginTop: 4,
+    marginRight: 80,
+  },
+});
+
+export default CreditCard;
